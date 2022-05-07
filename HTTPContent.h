08dcf37f63a2ed886cd84946c1e4a7b03556da3a
Elec42 <70710128+Elec42@users.noreply.github.com>
2022-05-07 20:43:11 +0200

    Update HTTPContent.h

--- HTTPContent.h
+++ HTTPContent.h
@@ -1,6 +1,6 @@
 /*
    HTTPContent.h - Library for HTTP Header/Body processing
-   Copyright(c) 2022 Felix Kröhnert. All right reserved.
+   Copyright(c) 2022 Felix Kröhnert. All rights reserved.
 
    This library is distributed under the terms of the <license> and WITHOUT ANY WARRANTY
 */
