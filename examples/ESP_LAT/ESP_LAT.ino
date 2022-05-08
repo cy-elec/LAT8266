80dc174f5fcce4161bca64ef492f5a2ad6f863e1
Felix Kröhnert WinWsl <felix.kroehnert@online.de>
2022-05-08 09:35:07 +0200

    renamed to LAT8266

--- examples/ESP_LAT/ESP_LAT.ino
+++ examples/ESP_LAT/ESP_LAT.ino
@@ -12,23 +12,12 @@ void setup() {
   
   LAT.WIFI_SSID = "ILR8OURxtkEI8D1p";
   LAT.WIFI_PSSWD = "Z1I25SWOmgqY3rSw";
-  if(LAT.wifi_connect(5000))
+  if(LAT.wifi_connect(5000)) {
     LAT.reflect("hthost=worldtimeapi.org;htpath=/api/timezone/Europe/Berlin;htdefault;htcode;htheader?;htbody?");
+  }
 }
 
 // the loop function runs over and over again forever
 void loop() {
   LAT.loop_cmd();
 }
-
-/*
-
-void print0(char *src, int l) {
-  for(int i=0; i<l; i++) {
-    if(*src==0) Serial.print("<NULL>");
-    else Serial.print(*src);
-    src++;
-  }
-  Serial.println();
-}
- */
