5601eca29fb44b76392d60d8da65470a40dbd557
Felix Kröhnert WinWsl <felix.kroehnert@online.de>
2022-07-19 00:14:55 +0200

    mistakes have been made

--- examples/helloWorld/helloWorld.ino
+++ examples/helloWorld/helloWorld.ino
@@ -12,7 +12,7 @@ void setup() {
   Serial.begin(LAT_BAUD); //enable Serial with 115200 BAUD. This can, however, be changed at will
   delay(500);
   LAT.MDNSName = "lat";
-  LAT.reflect("wifissid="+WSSID+";wifipasswd="+WPASSWD+";wificonn");
+  LAT.reflect("wifissid="+String(WSSID)+";wifipasswd="+String(WPASSWD)+";wificonn");
   Serial.println("LAT8266 play-ground\nType 'LAT' or 'VER' to test the functionality of this library\nTo access the Website enter 'http://lat:82' on your browser.");
 }
 
