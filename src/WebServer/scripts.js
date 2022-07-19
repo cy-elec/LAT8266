4bacaa849daac4ed0bade1e746fc1b3459dee3d7
Felix Kröhnert WinWsl <felix.kroehnert@online.de>
2022-07-19 18:44:23 +0200

    davestate

--- src/WebServer/scripts.js
+++ src/WebServer/scripts.js
@@ -4,9 +4,49 @@ function init() {
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
 			document.getElementById("favicon").href = xmlHttp.responseText;
-			document.getElementById("logoimg").src = document.getElementById("favicon").href;
+			let elements = [].slice.call(document.getElementsByClassName("logoimg"));
+			let hr = document.getElementById("favicon").href;
+			elements.forEach(element => {
+				element.src = hr;
+			});
 			console.log("Initialized...")
     }
     xmlHttp.open("GET", "image", true); // true for asynchronous 
     xmlHttp.send(null);
+
+	setContentOptions();
+	updateDisplay();
+}
+
+function updateDisplay() {
+	document.getElementById("selected_content").style.display="";
+	let elements = [].slice.call(document.getElementsByClassName("content"));
+	elements.forEach(element => {
+		if(element.id!="selected_content")
+			element.style.display="none";
+	});
+}
+
+function setContentOptions() {
+	let index_div = document.getElementById("index_div");
+	let elements = [].slice.call(document.getElementsByClassName("content"));
+	elements.forEach(element => {
+		let button = document.createElement("button");
+		button.innerHTML = element.getAttribute("name");
+		button.className = "content_button";
+		button.disabled = element.getAttribute("id")==="selected_content" ? true:false;
+		button.onclick = function() {
+			let buttons = [].slice.call(document.getElementsByClassName("content_button"));
+			buttons.forEach(btn => {
+				if(btn.innerHTML!= element.getAttribute("name"))
+					btn.disabled = false;
+				else
+					btn.disabled = true;
+			})
+			document.getElementById("selected_content").id="";
+			document.getElementsByName(element.getAttribute("name"))[0].id="selected_content";
+			updateDisplay();
+		};
+		index_div.appendChild(button);
+	});
 }
